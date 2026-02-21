c019dcb54b5ba45504aba2510fed70f072f7b9b2
Tuomas Tikka <tuomas.tikka@gmail.com>
2026-02-21 21:57:07 +0200

    update javadocs

--- src/com/tt/vectordb/config/Configuration.java
+++ src/com/tt/vectordb/config/Configuration.java
@@ -7,6 +7,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.Properties;
 
+/**
+ * Class for loading configuration properties and reading values.
+ *
+ * @author Tuomas Tikka
+ */
 public class Configuration {
 
     private static final Logger L = LoggerFactory.getLogger(Configuration.class);
@@ -17,6 +22,9 @@ public class Configuration {
 
     private Properties properties;
 
+    /**
+     * Constructor, where properties are loaded from files on disk.
+     */
     private Configuration() {
         File file = new File(System.getProperty("configuration", FILENAME));
         try {
@@ -29,10 +37,20 @@ public class Configuration {
         }
     }
 
+    /**
+     * Constructor, where properties are loaded from input parameters.
+     *
+     * @param properties The properties
+     */
     private Configuration(Properties properties) {
         this.properties = properties;
     }
 
+    /**
+     * Return the usable instance to read configuration properties.
+     *
+     * @return The instance
+     */
     public static Configuration getConfiguration() {
         if (configuration == null) {
             configuration = new Configuration();
@@ -40,6 +58,12 @@ public class Configuration {
         return (configuration);
     }
 
+    /**
+     * Return the usable instance to read configuration properties.
+     *
+     * @param properties The properties
+     * @return The instance
+     */
     public static Configuration getConfiguration(Properties properties) {
         if (configuration == null) {
             configuration = new Configuration(properties);
@@ -47,14 +71,29 @@ public class Configuration {
         return (configuration);
     }
 
+    /**
+     * Return a string value for a property.
+     *
+     * @param key The property key
+     * @param defaultValue A default value if property with 'key' is not found
+     * @return The string value for the property
+     */
     public String get(String key, String defaultValue) {
         return (properties.getProperty(key, defaultValue));
     }
 
+    /**
+     * Return an integer value for a property.
+     *
+     * @param key The property key
+     * @param defaultValue A default value if property with 'key' is not found
+     * @return The integer value for the property
+     */
     public int get(String key, int defaultValue) {
         try {
             return (Integer.parseInt(properties.getProperty(key)));
         } catch (Exception e) {
+            L.warn(String.format("error reading property with key '%s' as integer (%s) - using default value %d", key, e.getMessage(), defaultValue));
             return (defaultValue);
         }
     }
