9700b990a8b28bdb3a7fdf06ff9878e819fc3480
Tuomas Tikka <tuomas.tikka@gmail.com>
2026-02-21 12:12:33 +0200

    initial commit

---
+++ src/com/tt/vectordb/service/Similarity.java
@@ -0,0 +1,10 @@
+package com.tt.vectordb.service;
+
+import com.tt.vectordb.model.Search;
+import com.tt.vectordb.storage.VectorStoreItem;
+
+public interface Similarity {
+
+    double compare(Search search, VectorStoreItem item);
+
+}
