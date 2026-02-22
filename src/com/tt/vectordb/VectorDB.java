a8016e292537ae5258c80794394eb328bb7cb2f5
Tuomas Tikka <tuomas.tikka@gmail.com>
2026-02-22 11:17:22 +0200

    update javadocs

--- src/com/tt/vectordb/VectorDB.java
+++ src/com/tt/vectordb/VectorDB.java
@@ -7,35 +7,87 @@ import com.tt.vectordb.service.DBService;
 import java.util.Properties;
 import java.util.Random;
 
+/**
+ * Class representing one instance of VectorDB.
+ *
+ * @author Tuomas Tikka
+ */
 public class VectorDB {
 
+    /**
+     * Initialize an instance of VectorDB.
+     *
+     * @param properties The configuration properties object
+     * @throws Exception If the instance cannot be initialized
+     */
     public VectorDB(Properties properties) throws Exception {
         Configuration.getConfiguration(properties);
     }
 
+    /**
+     * Create an index.
+     *
+     * @param index The index object
+     * @return The index object (id populated)
+     * @throws Exception If the index cannot be created
+     */
     public Index createIndex(Index index) throws Exception {
         index.setId(System.currentTimeMillis());
         DBService.getService().createIndex(index);
         return (index);
     }
 
+    /**
+     * Return information for an index.
+     *
+     * @param id The index id
+     * @return The index object
+     * @throws Exception If the index cannot be returned
+     */
     public Index getIndex(long id) throws Exception {
         return (DBService.getService().getIndex(id));
     }
 
+    /**
+     * Delete an index.
+     *
+     * @param id The index id
+     * @throws Exception If the index cannot be deleted
+     */
     public void deleteIndex(long id) throws Exception {
         DBService.getService().deleteIndex(id);
     }
 
+    /**
+     * Create an entry into an index.
+     *
+     * @param id The index id
+     * @param entry The entry object
+     * @return The entry object
+     * @throws Exception If the entry cannot be created
+     */
     public Entry createEntry(long id, Entry entry) throws Exception {
         DBService.getService().createEntry(id, entry);
         return (entry);
     }
 
+    /**
+     * Search for entries in an index.
+     *
+     * @param id The index id
+     * @param search The search criteria object
+     * @return A search results object
+     * @throws Exception If the search cannot be performed
+     */
     public SearchResult searchEntries(long id, Search search) throws Exception {
         return (DBService.getService().searchEntries(id, search));
     }
 
+    /**
+     * Close the instance.
+     *
+     * @throws Exception If the instance cannot be closed
+     */
     public void close() throws Exception {
         DBService.getService().close();
     }
